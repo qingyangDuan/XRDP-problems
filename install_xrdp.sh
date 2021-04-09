dad917e9b336efd887f4f96a9ede4527b9f1abec
duanqingyang <33786097+duanqingyang@users.noreply.github.com>
2021-04-09 14:19:05 +0800

    Update install_xrdp.sh

--- install_xrdp.sh
+++ install_xrdp.sh
@@ -1,5 +1,9 @@
    sudo apt autoremove xrdp xorg
    
+   #switch to python3.6
+   sudo update-alternatives --config python3
+   sudo apt install python3-gi
+   
    # Add the repository and update apt
    sudo add-apt-repository ppa:martinx/xrdp-hwe-18.04
    sudo apt-get update
